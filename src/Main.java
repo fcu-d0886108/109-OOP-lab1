428e1acbf5769b4d43e8dba34e3094143d989e0b
user <user@EA01>
2020-09-29 09:40:39 +0800

    第二次

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I'm Yan PoHao");
+		System.out.println("My nid is D0886108");
+		System.out.println("This is lab 1 hw");
 	}
 
 }
